57846c2d9fbcf3cb8b640129e8ec5d179b681158
adevofficial311-dev <adevofficial311@gmail.com>
2026-08-26 12:58:39 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Navbar } from './components/layout/Navbar';
 import { SongPlayer } from './components/layout/SongPlayer';
 import { Charizard } from './components/ui/Charizard';
 import { FontProvider } from './context/FontContext';
+import { AuthProfileProvider } from './context/AuthProfileContext';
 import { FontChangerModal } from './components/ui/FontChangerModal';
+import { ProfileModal } from './components/ui/ProfileModal';
+import { AdminLoginModal } from './components/ui/AdminLoginModal';
 
 import { Home } from './pages/Home';
 import { Enter } from './pages/Enter';
@@ -22,28 +25,35 @@ export default function App() {
     return () => document.removeEventListener('contextmenu', handleContextMenu);
   }, []);
 
+  const [profileOpen, setProfileOpen] = useState(false);
+  const [adminOpen, setAdminOpen] = useState(false);
+
   return (
     <FontProvider>
-      <BrowserRouter>
-        <div className="min-h-screen bg-cb-bg flex flex-col relative text-cb-text">
-          <div className="relative z-10 flex flex-col min-h-screen">
-            <Navbar />
-            <main className="flex-1 flex flex-col">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/enter" element={<Enter />} />
-                <Route path="/macros" element={<Macros />} />
-                <Route path="/macro/:id" element={<ViewMacro />} />
-                <Route path="/publish" element={<PublishMacro />} />
-                <Route path="/creator/:name" element={<Profile />} />
-              </Routes>
-            </main>
-            <SongPlayer />
-            <Charizard />
-            <FontChangerModal />
+      <AuthProfileProvider>
+        <BrowserRouter>
+          <div className="min-h-screen bg-cb-bg flex flex-col relative text-cb-text">
+            <div className="relative z-10 flex flex-col min-h-screen">
+              <Navbar setProfileOpen={setProfileOpen} setAdminOpen={setAdminOpen} />
+              <main className="flex-1 flex flex-col">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/enter" element={<Enter />} />
+                  <Route path="/macros" element={<Macros />} />
+                  <Route path="/macro/:id" element={<ViewMacro />} />
+                  <Route path="/publish" element={<PublishMacro />} />
+                  <Route path="/creator/:name" element={<Profile />} />
+                </Routes>
+              </main>
+              <SongPlayer />
+              <Charizard />
+              <FontChangerModal />
+              <ProfileModal isOpen={profileOpen} onClose={() => setProfileOpen(false)} />
+              <AdminLoginModal isOpen={adminOpen} onClose={() => setAdminOpen(false)} />
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </AuthProfileProvider>
     </FontProvider>
   );
 }
